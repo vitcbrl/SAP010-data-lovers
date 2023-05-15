b2102b940e863ab600668151c4f27f8d7b27f1a5
Laboratoria/SAP010-card-validation <carolvas98@hotmail.com>
2023-05-15 09:46:04 -0300

    alteração na main

--- src/data.js
+++ src/data.js
@@ -4,21 +4,17 @@ const resultado = data.results; // aqui estou atribuindo os resultados do meu ar
 //crio uma function que a finalidade é ver se o personagem ta vivo ou morto.Coloco o export pois essa função vai ser usada no meu arquivoo main.js
 export function filterByStatus(status) {
   const filteredStatus = resultado.filter((resultado) => {
-    //aqui se usa uma arrow function e e estamos filtrando o personagem usando o metodo .filter essa expressão é para filtrar os personagens e retornar uma nova lista com personagens que atendam a esse critério
+    
     return resultado.status === status;
-    // nessa linha estamos acessando o estado de vida do personagem e verificando se os dois valores são iguais
   });
   return filteredStatus;
-  //aqui retorna true se for igual e mantem na lista e false se for diferente e descarta
+
 }
 
-//função com finalidade de ver a especie do personagem,a lista dos personagens que queremos filtrar e especies que é a especie que queremos filtrar a lista
 export function filterSpecies(species) {
-  //se usa o metodo filter para criar uma nova lista contendo apenas as especies cuja é igual a specie desejada
   const filteredSpecies = resultado.filter(
     (resultado) => resultado.species === species
   );
-  //aqui retorna a lista filtrada
   return filteredSpecies;
 }
 
@@ -29,19 +25,6 @@ export function filterGender(gender) {
   return filteredGender;
 }
 
-//função com finalidade de filtrar planeta,primeiro de personagens que queremos filtrar e o segundo planeta que queremos filtrar
-//export function filterPlanets(planet) {
-//const filteredPlanets = resultado.filter((resultado) => resultado.origin.name === planet
-//aqui usamos o metodo filter para criar uma nova lista dos planetas que queremos, o character representa cada elemento do meu array characters. o .origem.name é para acessar a propriedade name que tá dentro do objeto origin
-//);
-// return filteredPlanets;}
-
-/*const calculateGenderPercentage = (gender, array) => {
-  const filterGender = resultado.filter(
-    (resultado) => resultado.gender === gender
-  );
-  return ((filterGender.length / array.length) * 100).toFixed(2);
-};*/
 
 //Ordenação de A-Z
 export function sortAll(order, resultado) {
